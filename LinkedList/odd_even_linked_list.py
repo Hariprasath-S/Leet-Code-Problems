7f63acf0ecdabbb821ddbc2202b7be414022d4cc
Hariprasath-S <52788825+Hariprasath-S@users.noreply.github.com>
2021-03-18 15:44:13 +0530

    Create odd_even_linked_list.py

---
+++ LinkedList/odd_even_linked_list.py
@@ -0,0 +1,16 @@
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+        odd = head
+        even = head.next
+        evenhead = even
+        
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+            
+        odd.next = evenhead
+        return head
